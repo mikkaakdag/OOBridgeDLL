d56158a8b8c825029af0d87b3df1d630035a4cb8
Mikka Akdag <mia011@edu.zealand.dk>
2025-09-23 09:46:44 +0200

    Oprettelse af MC klasse med klassedefinitioner samt Unit Tests og brug af disse

---
+++ MC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOBridgeDLL;
+
+public class MC
+{
+    /// <summary>
+    /// MC klassen har 2 properties:
+    /// LicensePlate med typen string
+    /// Date med typen DateTime
+    /// </summary>
+    public string LicensePlate {  get; set; }
+    public DateTime Date {  get; set; }
+
+    /// <summary>
+    /// MC-klassens constructor har 2 parametre:
+    /// licensePlate af typen string, date af typen DateTime
+    /// Disse initialiseres med properties'ene
+    /// </summary>
+    public MC (string licensePlate, DateTime date)
+    {
+        LicensePlate = licensePlate;
+        Date = date;
+    }
+
+    /// <summary>
+    /// Der findes også 2 metoder:
+    /// Price og Vehicle
+    /// </summary>
+    public double Price()
+    {
+        return 120;
+    }
+
+    public string Vehicle()
+    {
+        return "MC";
+    }
+}
