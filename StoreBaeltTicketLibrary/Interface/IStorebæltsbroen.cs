c1187d22bf395165ce8844293c0aa6eef29654f7
Mikka Akdag <mia011@edu.zealand.dk>
2025-09-26 10:28:21 +0200

    Tilføjet repository og interface

---
+++ StoreBaeltTicketLibrary/Interface/IStorebæltsbroen.cs
@@ -0,0 +1,15 @@
+﻿using OOBridgeDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary.Interface;
+
+public interface IStorebæltsbroen
+{
+    void AddTicket (Vehicle ticket);                  
+    List<Vehicle> GetAllTickets();                   
+    List<Vehicle> GetTicketsByLicense (string plate); 
+}
---
+++ StoreBaeltTicketLibrary/Repository/Storebæltsbroen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOBridgeDLL;
+
+namespace StoreBaeltTicketLibrary.Repository;
+
+public class Storebæltsbroen
+{
+    private static readonly List<Vehicle> _tickets = new List<Vehicle>();
+
+    public void AddTickets (Vehicle ticket)
+    {
+        _tickets.Add(ticket);
+    }
+
+    public List<Vehicle> GetAllTickets()
+    {
+        return _tickets; 
+    }
+
+    // Hent billetter efter nummerplade
+    public List<Vehicle> GetTicketsByLicense (string plate)
+    {
+        return _tickets
+            .Where(t => t.LicensePlate.Equals(plate, StringComparison.OrdinalIgnoreCase))
+            .ToList();
+    }
+}
